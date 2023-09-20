dd7bdc72dfbc04a962ce2ff46e0048a44155b110
Lulox <ligma@duck.com>
2023-09-19 22:16:24 -0300

    First commit

---
+++ packages/hardhat/deploy/00_deploy_streamer.js
@@ -0,0 +1,30 @@
+// deploy/00_deploy_streamer.js
+
+const { ethers } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("Streamer", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    log: true,
+  });
+
+  const streamer = await ethers.getContract("Streamer", deployer);
+
+  console.log("\n 🤹  Sending ownership to frontend address...\n");
+  // Checkpoint 2: change address to your frontend address vvvv
+  const ownerTx = await streamer.transferOwnership(
+    "0xC8942a628E361F2BE5015fd80dC22c72b2F07373"
+  );
+
+  console.log("\n       confirming...\n");
+  const ownershipResult = await ownerTx.wait();
+  if (ownershipResult) {
+    console.log("       ✅ ownership transferred successfully!\n");
+  }
+};
+
+module.exports.tags = ["Streamer"];
